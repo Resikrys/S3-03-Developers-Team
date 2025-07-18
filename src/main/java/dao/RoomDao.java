d2eb7e99300153598e260f087b3b0d7943c37ae4
resikrys <resicris@gmail.com>
2025-07-18 10:39:32 +0200

    add Room.java model & RoomDAO interface

---
+++ src/main/java/dao/RoomDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomDao {
+    void createRoom(Room room) throws SQLException;
+    Optional<Room> getRoomById(int id) throws SQLException;
+    List<Room> getAllRooms() throws SQLException;
+    void updateRoom(Room room) throws SQLException;
+    void deleteRoom(int id) throws SQLException;
+}
---
+++ src/main/java/model/Room.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Room {
+    private int id;
+    private String theme;
+    private int difficulty_level;
+
+    public Room(int id, String theme, int difficulty_level) {
+        this.id = id;
+        this.theme = theme;
+        this.difficulty_level = difficulty_level;
+    }
+
+    public Room(String theme, int difficulty_level) {
+        this(0, theme, difficulty_level); // delega en el constructor completo
+    }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getTheme() {return theme;}
+    public void setTheme(String theme) {this.theme = theme;}
+    public int getDifficulty_level() {return difficulty_level;}
+    public void setDifficulty_level(int difficulty_level) {this.difficulty_level = difficulty_level;}
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", theme='" + theme + '\'' +
+                ", capacity=" + difficulty_level +
+                '}';
+    }
+}
