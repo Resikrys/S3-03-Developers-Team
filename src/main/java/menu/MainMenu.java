b4296996cf04b5f6795de25f6b402f7e4bb102bd
resikrys <resicris@gmail.com>
2025-07-21 13:45:26 +0200

    copy & add Ana's mainMenu version

--- src/main/java/menu/MainMenu.java
+++ src/main/java/menu/MainMenu.java
@@ -2,6 +2,7 @@ package menu;
 
 import dbconnection.DatabaseConnection;
 import dbconnection.EnvLoader;
+import manager.EscapeRoomManager;
 import manager.RoomManager;
 import util.InputHelper;
 import manager.ClueManager;
@@ -13,11 +14,16 @@ public class MainMenu {
     private final RoomManager roomManager;
     private final ClueManager clueManager;
     private final DecorationManager decorationManager;
+    private final EscapeRoomManager escaperoomManager;
+//    private final PlayerManager playermanager;
+//    private final Ticketmanager ticketManager;
+//    private final Inventory inventory;
 
     public MainMenu() {
         EnvLoader.getInstance();
 
         this.inputHelper = new InputHelper();
+        this.escaperoomManager = new EscapeRoomManager(inputHelper);
         this.roomManager = new RoomManager(inputHelper);
         this.clueManager = new ClueManager(inputHelper);
         this.decorationManager = new DecorationManager(inputHelper);
@@ -29,21 +35,37 @@ public class MainMenu {
         try {
             do {
                 System.out.println("\n--- Main Menu Escape Room ---");
-                System.out.println("1. CRUD Operations -> Rooms");
-                System.out.println("2. CRUD Operations -> Clues");
-                System.out.println("3. CRUD Operations -> Decoration Objects");
+                System.out.println("1. CRUD Operations -> EscapeRoom");
+                System.out.println("2. CRUD Operations -> Rooms");
+                System.out.println("3. CRUD Operations -> Clues");
+                System.out.println("4. CRUD Operations -> Decoration Objects");
+                System.out.println("5. CRUD Operations -> Player");
+                System.out.println("6. CRUD Operations -> Ticket");
+                System.out.println("7. CRUD Operations -> Inventory");
                 System.out.println("0. Exit");
                 input = inputHelper.readInt("Select option: ");
 
                 switch (input) {
                     case 1:
-                        roomManager.handleRoomCrud();
+                        escaperoomManager.handleEscapeRoomCrud(); //Change to showMenu when submenu available
                         break;
                     case 2:
-                        clueManager.handleClueCrud();
+                        roomManager.showMenu();
                         break;
                     case 3:
-                        decorationManager.handleDecorationCrud();
+                        clueManager.showMenu();
+                        break;
+                    case 4:
+                        decorationManager.showMenu();
+                        break;
+                    case 5:
+                        //playerManager.showMenu();
+                        break;
+                    case 6:
+                        //ticketManager.showMenu();
+                        break;
+                    case 7:
+                        //inventory.showMenu();
                         break;
 
                     case 0:
