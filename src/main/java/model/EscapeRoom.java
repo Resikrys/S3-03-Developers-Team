eb0628e9f84a8e7fca23cc76cbb26d984c00f029
resikrys <resicris@gmail.com>
2025-07-21 12:44:48 +0200

    copy Escaperoom model & DAO

---
+++ src/main/java/model/EscapeRoom.java
@@ -0,0 +1,44 @@
+package model;
+
+public class EscapeRoom {
+    private int id;
+    private String name;
+    private int totalTickets;
+
+    public EscapeRoom() {}
+
+    public EscapeRoom(String name, int totalTickets) {
+        this.name = name;
+        this.totalTickets = totalTickets;
+    }
+
+    public EscapeRoom(int id, String name, int totalTickets) {
+        this.id = id;
+        this.name = name;
+        this.totalTickets = totalTickets;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+}
